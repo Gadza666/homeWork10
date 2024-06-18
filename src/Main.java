900a5cba4470be355cf6f3ce89a1594e6e4182f2
Gadza88 <vasiliew.renat@gmail.com>
2024-06-19 00:13:10 +0300

    fix doneV2.1

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,16 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Task 1");
-        printIsLeapYear(2003);
+        printIsLeapYear(2002);
 
         System.out.println("Task 2");
         int currentYear = LocalDate.now().getYear();
         printValidTypeInfo(1, 2000);
 
         System.out.println("Task 3");
-        calculateDeliveryDays(67);
+        System.out.println("Потребуется дней:" + calculateDeliveryDays(55));
+
+
     }
 
     public static void printIsLeapYear(int year) {
@@ -26,13 +28,13 @@ public class Main {
     }
 
     public static void printValidTypeInfo(int type, int year) {
-        if (type == 0 && year >= 2015) {
+        if (type == 0 && year >= 2024) {
             System.out.println("Установите версию приложения для iOS по ссылке ");
-        } else if (type == 0 && year < 2015) {
+        } else if (type == 0 && year < 2024) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (type == 1 && year >= 2015) {
+        } else if (type == 1 && year >= 2024) {
             System.out.println("Установите версию приложения для Android по ссылке ");
-        } else if (type == 1 && year < 2015) {
+        } else if (type == 1 && year < 2024) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке ");
         }
     }
@@ -41,19 +43,17 @@ public class Main {
         int day = 0;
         if (deliveryDistance <= 20) {
             day++;
-            System.out.println("Потребуется " + day + " день что бы доставить груз на " + deliveryDistance + " км.");
-        } else if
-        (deliveryDistance > 20 && deliveryDistance < 60) {
+        }
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
             day += 2;
-            System.out.println("Потребуется " + day + " дня для доставки на " + deliveryDistance + " км.");
-        } else if
-        (deliveryDistance >= 60 && deliveryDistance <= 100) {
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
             day += 3;
-            System.out.println("Потребуется " + day + " дня для доставки на " + deliveryDistance + " км.");
-        } else {
-            System.out.println("Доставка нет на " + deliveryDistance + " км. слишком большое расстояние");
+        }
+        if (deliveryDistance > 100) {
+            return -1;
         }
         return day;
     }
-
 }
+
